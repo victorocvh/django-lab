a444a1af68bf162d061dffc467307997d9c302d8
Victor Carvalho <victoro.ocv@hotmail.com>
2024-01-24 06:20:31 -0300

    adjuste: removing sqlite file

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ O projeto `django-solid` é uma aplicação web construída utilizando o framewo
 ## Tecnologias e Conceitos Utilizados
 
 - **Django**: Framework web de alto nível para desenvolvimento rápido e limpo.
+- **Docker**: Containerização da aplicação, permitindo a entrada de novos colaboradores de maneira fácil, e isolamento das dependências do projeto.
 - **Clean Code**: Adoção de boas práticas para garantir código claro, legível e de fácil manutenção.
 - **SOLID Principles**: Aplicação dos princípios SOLID para promover a flexibilidade e extensibilidade do código.
 - **12 Fatores**: Implementação dos 12 fatores para construir aplicações robustas, escaláveis e fáceis de gerenciar.
+
 
 ## Práticas Adotadas
 
--- django_web/django_web/settings.py
+++ django_web/django_web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'main'
 ]
 
 MIDDLEWARE = [
@@ -74,12 +75,15 @@ WSGI_APPLICATION = 'django_web.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django-web",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
